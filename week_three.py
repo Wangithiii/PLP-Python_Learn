f1f0d3b1854874346f453e704fa3eed8300427fb
Wangithiii <ym.wangithi@gmail.com>
2024-12-16 13:10:34 +0300

    ASsignment complete

---
+++ week_three.py
@@ -0,0 +1,23 @@
+def calculate_discount(price, discount_percent):
+    
+    if discount_percent >= 20:
+        discount_amount = (discount_percent / 100) * price
+        return price - discount_amount
+    else:
+        return price
+
+# Prompt the user for input
+try:
+    original_price = float(input("Input original price: "))
+    discount_percentage = float(input("Enter the discount percentage: "))
+
+    final_price = calculate_discount(original_price, discount_percentage)
+
+    # Print the result
+    if discount_percentage >= 20:
+        print(f"The final price after applying the discount is: ${final_price:.2f}")
+    else:
+        print(f"No discount applied. The original price is: ${original_price:.2f}")
+
+except ValueError:
+    print("Please enter valid numeric values for price and discount percentage.")
